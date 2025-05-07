a7f7bcf664533f0a9996c028b2c9dea9ebdcb19a
Sam-Waithaka <sw.waithakasamuel@gmail.com>
2025-05-07 11:42:00 +0300

    rewrite the whole app using vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { FaRegTrashAlt } from "react-icons/fa";
+
+function App() {
+  
+
+  return (
+    <>
+    <FaRegTrashAlt />
+      <h1>Your Appointments</h1>
+    </>
+  )
+}
+
+export default App
