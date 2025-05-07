d02f2886a47e4e1f6d02d07cd175b9d75b47a330
Sam-Waithaka <sw.waithakasamuel@gmail.com>
2025-05-07 11:06:04 +0300

    Using react icon components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { FaRegTrashCan } from "react-icons/fa6";
+
 function App() {
   return (
     <div className="App">
-      <h1>Your Appointments</h1>
+      <h1> <FaRegTrashCan/>Your Appointments</h1>
     </div>
   );
 }
